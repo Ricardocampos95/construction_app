dcd78b931a2897e53256470b5f2b82764829d378
Ricardocampos95 <ricardo.eteo@gmail.com>
2026-06-06 14:50:20 +0100

    chore: initial project setup

---
+++ src/main/java/com/ricardocampos/constructionapp/ConstructionAppApplication.java
@@ -0,0 +1,13 @@
+package com.ricardocampos.constructionapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConstructionAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConstructionAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ricardocampos/constructionapp/ConstructionAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ricardocampos.constructionapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConstructionAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
