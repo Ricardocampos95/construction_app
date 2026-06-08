d2a9855c8a30ce8d4fe5d9f534046805bac77a95
Ricardocampos95 <ricardo.eteo@gmail.com>
2026-06-08 19:32:31 +0100

    refactor(product): drop quantity attribute and add unique reference

--- src/main/java/com/ricardocampos/constructionapp/entities/Product.java
+++ src/main/java/com/ricardocampos/constructionapp/entities/Product.java
@@ -33,8 +33,8 @@ public class Product {
     @Column(name = "unit", nullable = false)
     private UnitType unitType;
 
-    @Column(name = "quantity", nullable = false, precision = 10, scale = 2)
-    private BigDecimal quantity;
+    @Column(name = "reference", nullable = false, unique = true)
+    private String reference;
 
     @Column(name = "min_stock_alert", nullable = false, precision = 10, scale = 2)
     private BigDecimal minStockAlert;
